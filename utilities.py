c431c9865c59563de26c99ff29b94e6df28d583c
Tural Aksel <turalaksel@gmail.com>
2018-11-12 19:35:37 -0800

    Add new files

---
+++ utilities.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2018-11-09 13:35:55
+# @Author  : Your Name (you@example.org)
+# @Link    : http://example.org
+# @Version : $Id$
+
+# EM to Star map
