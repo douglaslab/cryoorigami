Fix normalization by frc

origamiem.py
@@ -1373,6 +1373,11 @@ class ProjectSubtract2D(Project):
                           'cropctf': parallelem.crop_class_ctf,
                           'crop':    parallelem.crop_class}
 
+        # Other mask parameters
+        self.inner_diameter   = None
+        self.inner_radius_pix = None
+        self.inner_mask       = None
+
     def write_results(self):
         '''
         Write results
@@ -1482,6 +1487,11 @@ class ProjectSubtract2D(Project):
             if self.mask_subtract_mrc_file is None:
                 self.mask_subtract_mrc = MRC()
                 self.mask_subtract_mrc.set_img2D(self.mask_structure_mrc.get_img2D())
+
+                # If inner mask exists, use that to create subtract mask
+                if self.inner_mask is not None:
+                    self.mask_subtract_mrc.apply_mask(1-self.inner_mask)
+
                 self.mask_subtract_mrc.store_to_original()
 
             for ptcl_index, ptcl_row in particle_data.iterrows():
@@ -1566,6 +1576,12 @@ class ProjectSubtract2D(Project):
         # Store the originals of masks
         self.mask_align_mrc.store_to_original()
 
+    def set_inner_diameter(self, inner_diameter):
+        '''
+        Set inner diameter
+        '''
+        self.inner_diameter = inner_diameter
+
     def prepare_project(self):
         '''
         Prepare project
@@ -1610,6 +1626,17 @@ class ProjectSubtract2D(Project):
             # Make the mask soft
             self.circular_mask = scipy.ndimage.filters.gaussian_filter(self.circular_mask, sigma)
 
+    def create_inner_mask(self):
+        '''
+        Use inner diameter to create an outer mask
+        '''
+        if self.inner_diameter is not None:
+            self.inner_radius_pix = int(0.5*self.inner_diameter/self.particle_apix)
+
+            # Create inner mask
+            self.inner_mask = util.circular_mask(self.first_particle_mrc.img2D.shape,
+                                                 center=None,
+                                                 radius=self.inner_radius_pix)
 
     def prepare_meta_objects(self):
         '''
@@ -1619,6 +1646,7 @@ class ProjectSubtract2D(Project):
         self.create_output_subtract_mrc()
         self.create_output_subtract_star()
         self.create_circular_mask()
+        self.create_inner_mask()
 
 
 class ProjectGroup(Project):
@@ -2257,7 +2285,15 @@ class MRC:
 
             # Determine r
             spacing = 1.0/(self.img2D.shape[0]*apix)
-            self.ctf_r  = np.round(self.ctf_s/spacing)*spacing
+            self.ctf_r = np.round(self.ctf_s/spacing)
+
+            # Get half bin number
+            quart_n = self.img2D.shape[0] // 4
+            mask    = self.ctf_r > quart_n
+            self.ctf_r[mask] = quart_n + 1 
+
+            # Change ctf_r data dtype
+            self.ctf_r = np.array(self.ctf_r, dtype=int)
 
     def get_ctf_r(self):
         '''

parallelem.py
@@ -300,37 +300,72 @@ def eval_ctf(ctf_s, ctf_a, defU, defV, defA=0, phaseShift=0, kv=300, ac=0.1, cs=
 
     return img2D_ctf
 
-def calc_frc(current_fft, other_fft, ctf_r):
+def calc_frc(current_fft, ref_fft, ctf_r):
     '''
     Compute frc between two ft
     '''
     frc2D = np.ones(current_fft.shape, dtype=np.float32)
     rbins = np.sort(np.unique(ctf_r))
+    nbins = len(rbins)
+    max_r = np.max(rbins)
 
     # Get cross correlations
-    cross_cc = current_fft*np.conj(other_fft)
-    self1_cc = np.abs(current_fft)**2
-    self2_cc = np.abs(other_fft)**2
-
-    # Quarter of  the number of bins 
-    # Merge two bins to single bin and work on half the fourier space
-    half_nbins = len(rbins)//4
+    cross_cc = current_fft*np.conj(ref_fft)
+    ref_cc   = np.abs(ref_fft)**2
 
     # Calculate for half the FFT
-    for i in range(half_nbins):
-        mask = (ctf_r == rbins[2*i]) | (ctf_r == rbins[2*i+1])
+    for i in range(nbins-1):
+        mask  = ctf_r == rbins[i]
         corr  = np.sum(cross_cc[mask])
-        norm1 = np.sum(self1_cc[mask])
-        norm2 = np.sum(self2_cc[mask])
+        norm  = np.sum(ref_cc[mask])
 
-        frc2D[mask] = np.abs(corr/np.sqrt(norm1*norm2))
+        frc2D[mask] = np.abs(corr/norm)
 
-    # For the rest assign it to 0
-    mask = ctf_r >= rbins[2*half_nbins]
+    # For the rest assign it to 1
+    mask = ctf_r == max_r
     frc2D[mask] = 1.0
     
     return frc2D
 
+@jit(nopython=True)
+def calc_frc_numba(current_fft, ref_fft, ctf_r):
+    '''
+    Compute frc between two ft
+    '''
+    frc2D = np.ones(current_fft.shape)
+    max_r = np.max(ctf_r)
+
+    # Initialize arrays
+    corr_sum = np.zeros(max_r+1)
+    self_sum = np.zeros(max_r+1) 
+
+    # Get cross correlations
+    cross_cc = np.abs(current_fft*np.conj(ref_fft))
+    ref_cc   = np.abs(ref_fft)**2
+
+    # Calculate for half the FFT
+    for i in range(frc2D.shape[0]):
+        for j in range(frc2D.shape[1]):
+            r = ctf_r[i ,j]
+            corr_sum[r] += cross_cc[i, j]
+            self_sum[r] += ref_cc[i, j]
+
+    # Assign to final bin 
+    corr_sum[max_r] = 1.0
+    self_sum[max_r] = 1.0
+
+    # Determine norm sums
+    norm_sum = corr_sum/self_sum
+
+    # Assign values
+    for i in range(frc2D.shape[0]):
+        for j in range(frc2D.shape[1]):
+            r = ctf_r[i,j]
+            
+            # Assign the new value
+            frc2D[i,j] = norm_sum[r]
+    
+    return frc2D
 
 def normalize_frc(img2D_fft, frc2D):
     '''
@@ -383,7 +418,6 @@ def calc_mean_std_intensity(img2D, mask):
         std_intensity  = np.std(img2D)
 
     return mean_intensity, std_intensity
-
 
 def threshold_above(img2D, threshold=1.0):
     '''
@@ -439,7 +473,6 @@ def subtract_class_ctf(class_img2D, ctf_a, ctf_s, ctf_r, ptcl_star, mask_align_i
     elif norm_method == 'intensity':
         masked_class_img2D = ifft_img2D(class_fft2D)
         real_coeff = calc_intensity_ratio(ptcl_img2D, masked_class_img2D)
-
 
     ptcl_img2D  = subtract_class_from_ptcl(class_img2D, class_ctf, ptcl_img2D, mask_subtract_img2D, fft_coeff, real_coeff)
 